f0bf3dc055109369142d1f352357fd76b547f789
cooolbros <pfwobcke@gmail.com>
2025-05-03 22:40:12 +1000

    Update to tRPC 11

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,31 +1,16 @@
 import { appRouter } from "$lib/server/TRPCAppRouter"
 import type { Handle } from "@sveltejs/kit"
-import type { HTTPHeaders } from "@trpc/client"
-import { resolveHTTPResponse } from "@trpc/server/http"
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 
 export const handle: Handle = async ({ event, resolve }) => {
 
 	if (event.url.pathname.startsWith("/api/trpc/")) {
-
-		const result = await resolveHTTPResponse({
-			createContext: async () => ({ event }),
-			path: event.url.pathname.substring("/api/trpc/".length),
-			req: {
-				body: await event.request.text(),
-				headers: <HTTPHeaders><unknown>event.request.headers,
-				method: event.request.method,
-				query: event.url.searchParams
-			},
-			router: appRouter
+		return await fetchRequestHandler<typeof appRouter>({
+			router: appRouter,
+			createContext: () => ({ event }),
+			req: event.request,
+			endpoint: "/api/trpc/"
 		})
-
-		return new Response(
-			result.body,
-			{
-				headers: <HeadersInit><unknown>result.headers,
-				status: result.status
-			}
-		)
 	}
 
 	return resolve(event)
--- src/lib/client/TRPCClient.ts
+++ src/lib/client/TRPCClient.ts
@@ -1,7 +1,7 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
+import { createTRPCClient, httpBatchLink } from "@trpc/client"
 import type { AppRouter } from "../server/TRPCAppRouter"
 
-export const trpc = createTRPCProxyClient<AppRouter>({
+export const trpc = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
 			url: "/api/trpc"
